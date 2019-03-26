3c88dce69e888026c186d21e7fbe9aecef8854fb
Paolo Leopardi <leopardipaolo@gmail.com>
2019-03-26 22:13:45 +0100

    refactor(SwipeAnimationPage):
    upgrade to new AnimationContainer interface

--- lib/SwipeAnimation/swipeAnimation.page.dart
+++ lib/SwipeAnimation/swipeAnimation.page.dart
@@ -1,11 +1,14 @@
 import "package:flutter/material.dart";
 import "./animation.container.dart";
+import "./item.dart";
 
 class SwipeAnimationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimationContainer(),
+      body: AnimationContainer(
+        child: Item(),
+      ),
     );
   }
 }
