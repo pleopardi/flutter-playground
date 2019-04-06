6d1eb26258064493c3a23dccea009cbdc236bd44
Paolo Leopardi <leopardipaolo@gmail.com>
2019-04-06 12:04:23 +0200

    feat(ItemContainer):
    basic dragging implementation

--- lib/SwipeAnimation/item.container.dart
+++ lib/SwipeAnimation/item.container.dart
@@ -21,12 +21,32 @@ class ItemContainer extends StatefulWidget {
 class _ItemContainerState extends State<ItemContainer> {
   final double _cardHeight = 200.0;
   final double _cardWidth = 200.0;
+  double _dx = 0.0;
+  double _dy = 0.0;
+
+  void handlePanEnd(DragEndDetails details) {
+    setState(() {
+      _dx = 0;
+      _dy = 0;
+    });
+  }
+
+  void handlePanStart(DragStartDetails details) {}
+
+  void handlePanUpdate(DragUpdateDetails details) {
+    setState(() {
+      _dx = _dx + details.delta.dx;
+      _dy = _dy + details.delta.dy;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final _pageSize = MediaQuery.of(context).size;
 
     return GestureDetector(
+      onPanEnd: handlePanEnd,
+      onPanUpdate: handlePanUpdate,
       onTap: widget.animateForward,
       child: Container(
         alignment: Alignment.center,
@@ -35,8 +55,11 @@ class _ItemContainerState extends State<ItemContainer> {
           children: <Widget>[
             Positioned(
               child: widget.child(),
-              left: _pageSize.width / 2 - _cardWidth / 2 - widget.delta,
-              top: _pageSize.height / 2 - _cardHeight / 2 - widget.delta / 3,
+              left: _pageSize.width / 2 - _cardWidth / 2 - widget.delta + _dx,
+              top: _pageSize.height / 2 -
+                  _cardHeight / 2 -
+                  widget.delta / 3 +
+                  _dy,
             ),
           ],
         ),
