729ab835df2e9e5367001b95aff10d7e9d539f8e
Paolo Leopardi <leopardipaolo@gmail.com>
2019-04-07 11:41:50 +0200

    feat(CardPilePage):
    add basic hardcoded cards pile

--- lib/CardsPileExperiment/cardsPile.page.dart
+++ lib/CardsPileExperiment/cardsPile.page.dart
@@ -7,7 +7,41 @@ class CardsPilePage extends StatelessWidget {
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
-        child: Item(),
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: <Widget>[
+            Positioned(
+              bottom: 50.0,
+              child: Item(
+                color: Colors.blue,
+                height: 120.0,
+                width: 160.0,
+              ),
+            ),
+            Positioned(
+              bottom: 25.0,
+              child: Item(
+                color: Colors.green,
+                height: 135.0,
+                width: 180.0,
+              ),
+            ),
+            Positioned(
+              bottom: 0.0,
+              child: GestureDetector(
+                onTap: () {
+                  print("Tap");
+                },
+                child: Item(
+                  color: Colors.red,
+                  height: 150.0,
+                  width: 200.0,
+                ),
+              ),
+            ),
+            Container(),
+          ],
+        ),
       ),
     );
   }
