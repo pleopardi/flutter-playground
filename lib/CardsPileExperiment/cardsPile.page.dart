637f2c5d4caad2c2cef950b63307a9e3cc32345c
Paolo Leopardi <leopardipaolo@gmail.com>
2019-04-10 09:12:11 +0200

    feat(CardsPilePage):
    basic pile rendering

--- lib/CardsPileExperiment/cardsPile.page.dart
+++ lib/CardsPileExperiment/cardsPile.page.dart
@@ -1,20 +1,40 @@
 import "package:flutter/material.dart";
 import "./item.dart";
 import "./helpers.dart";
+import "../SwipeAnimationExperiment/SwipeAnimation/swipeAnimation.dart";
 
 class CardsPilePage extends StatelessWidget {
-  List<Widget> generateItemsPile(int count) {
+  List<Widget> generateItemsPile(int count, double pageWidth) {
     final itemsData = generateItemsData(
       count: count,
       deltaBottom: 20.0,
-      initialHeight: 200.0,
-      initialWidth: 300.0,
       deltaHeight: 10.0,
       deltaWidth: 15.0,
+      initialHeight: 200.0,
+      initialWidth: 300.0,
     );
 
     List<Widget> itemsPile = List<Widget>.generate(count, (int index) {
       final itemData = itemsData[index];
+
+      if (index == 0) {
+        print("Width: ${itemData.width}");
+        print("Page width: $pageWidth");
+        print("Left: ${pageWidth / 2 - itemData.width / 2}");
+
+        return SwipeAnimation(
+          bottom: itemData.bottom,
+          handleDismiss: (_) => print("Dismiss"),
+          handleSave: (_) => print("Save"),
+          left: (pageWidth - itemData.width) / 2,
+          renderChild: () => Item(
+                color: itemData.color,
+                height: itemData.height,
+                text: itemData.text,
+                width: itemData.width,
+              ),
+        );
+      }
 
       return Positioned(
         bottom: itemData.bottom,
@@ -27,19 +47,24 @@ class CardsPilePage extends StatelessWidget {
       );
     });
 
-    itemsPile.add(Container());
+    itemsPile.add(Container(
+      height: 600,
+      width: pageWidth,
+    ));
 
     return itemsPile.reversed.toList();
   }
 
   @override
   Widget build(BuildContext context) {
+    final double _pageWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
         child: Stack(
           alignment: Alignment.bottomCenter,
-          children: generateItemsPile(10),
+          children: generateItemsPile(10, _pageWidth),
         ),
       ),
     );
