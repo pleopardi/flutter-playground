114725837035b99db425bb12f5498fa723569a0b
Paolo Leopardi <leopardipaolo@gmail.com>
2019-04-14 00:46:20 +0200

    refactor(routes):
    add route factory

--- lib/routes.dart
+++ lib/routes.dart
@@ -5,10 +5,14 @@ import "./DismissibleItemsListExperiment/dismissibleItemsList.page.dart";
 import "./GeocodingExperiment/geocoding.page.dart";
 import "./SwipeAnimationExperiment/swipeAnimation.page.dart";
 
+Function route(Widget page) {
+  return (BuildContext context) => page;
+}
+
 Map<String, WidgetBuilder> routes = {
-  "/": (BuildContext context) => HomePage(),
-  "/cardsPile": (BuildContext context) => CardsPilePage(),
-  "/dismissibleItemsList": (BuildContext context) => DismissibleItemsListPage(),
-  "/geocoding": (BuildContext context) => GeocodingPage(),
-  "/swipeAnimation": (BuildContext context) => SwipeAnimationPage(),
+  "/": route(HomePage()),
+  "/cardsPile": route(CardsPilePage()),
+  "/dismissibleItemsList": route(DismissibleItemsListPage()),
+  "/geocoding": route(GeocodingPage()),
+  "/swipeAnimation": route(SwipeAnimationPage()),
 };
