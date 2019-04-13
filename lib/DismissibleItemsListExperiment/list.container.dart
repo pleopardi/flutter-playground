fc289376a04eae23b83c2dc61e3cc1b06ffc7d6d
Paolo Leopardi <leopardipaolo@gmail.com>
2019-04-13 20:15:22 +0200

    feat(DismissibleItemsListExperiment):
    improve business logic

--- lib/DismissibleItemsListExperiment/list.container.dart
+++ lib/DismissibleItemsListExperiment/list.container.dart
@@ -12,8 +12,34 @@ class ItemsListContainer extends StatefulWidget {
 }
 
 class _ItemsListContainerState extends State<ItemsListContainer> {
+  List<String> dismissed = [];
+  List<String> saved = [];
+
+  void onDismissed({
+    @required DismissDirection direction,
+    @required int index,
+    @required String title,
+  }) {
+    if (direction == DismissDirection.startToEnd) {
+      setState(() {
+        saved.add(title);
+        widget._itemsList.removeAt(index);
+      });
+
+      return;
+    }
+
+    setState(() {
+      dismissed.add(title);
+      widget._itemsList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ItemsList(itemsList: widget._itemsList);
+    return ItemsList(
+      itemsList: widget._itemsList,
+      onDismissed: onDismissed,
+    );
   }
 }
--- lib/DismissibleItemsListExperiment/list.dart
+++ lib/DismissibleItemsListExperiment/list.dart
@@ -4,15 +4,21 @@ import "./listItem.dart";
 class ItemsList extends StatelessWidget {
   @required
   final List<dynamic> itemsList;
+  @required
+  final Function onDismissed;
 
-  ItemsList({this.itemsList});
+  ItemsList({this.itemsList, this.onDismissed});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: itemsList.length,
       itemBuilder: (BuildContext context, int index) {
-        return ListItem(itemsList[index]);
+        return ListItem(
+          index: index,
+          itemData: itemsList[index],
+          onDismissed: onDismissed,
+        );
       },
     );
   }
